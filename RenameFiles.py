ca1b46f8e585db16079ec93ed975ac6693763214
ManhNH <manhnh97@gmail.com>
2022-07-26 08:42:49 +0700

    Rename files in Folder with FileType

--- RenameFiles.py
+++ RenameFiles.py
@@ -1,22 +1,18 @@
 from pathlib import Path
 
-def rename_photos():
+def rename_photos(directory, extension_file, new_name):
     # Folder contains files need rename
-    DIR_PATH = Path("F:\Images_Test")
-    # Group file need replace
-    EXTENSION_FILES = "."+"jpg"
-    # Set file name need replace
-    FILE_NAME = "Test1"
+    DIR_PATH = Path(directory)
     # Set number start to replace
-    counter = 5
+    counter = 1
     
     for file in DIR_PATH.iterdir():
-        if file.suffix.lower() == EXTENSION_FILES.lower():
-            new_file = FILE_NAME + str(counter) + file.suffix
+        if file.suffix.lower() == '.'+extension_file.lower():
+            new_file = new_name + str(counter) + file.suffix
             file.rename(DIR_PATH / new_file)
             counter += 1
             
     print("Rename Success!!!")
 
 if __name__ == "__main__":
-    rename_photos()
+    rename_photos("F:\images_IT", "jpg", "TestRename")
