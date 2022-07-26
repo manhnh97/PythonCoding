fdafa5ae588b5611b9502bb2e5756a6a9db7a5f7
ManhNH <manhnh97@gmail.com>
2022-07-26 09:06:27 +0700

    Add try-except FileExistsError

--- RenameFiles.py
+++ RenameFiles.py
@@ -4,15 +4,19 @@ def rename_photos(directory, extension_file, new_name):
     # Folder contains files need rename
     DIR_PATH = Path(directory)
     # Set number start to replace
-    counter = 1
+    counter = 10
     
     for file in DIR_PATH.iterdir():
         if file.suffix.lower() == '.'+extension_file.lower():
-            new_file = new_name + str(counter) + file.suffix
-            file.rename(DIR_PATH / new_file)
-            counter += 1
-            
-    print("Rename Success!!!")
+            try:
+                new_file = f"{new_name}{counter}{file.suffix}"
+                file.rename(DIR_PATH / new_file)
+                print(f"Renaming {file.stem} to {new_file}")
+                counter += 1
+            except FileExistsError as e:
+                print(f"- File name \"{file.stem}\" already exists")
+            finally:
+                print("========== Rename Success!!! ==========")
 
 if __name__ == "__main__":
     rename_photos("F:\images_IT", "jpg", "TestRename")
